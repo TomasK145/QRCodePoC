55c6f0eb3f3a9ffcec68e399821f3dc4cf9571f3
Tomas Krchnak <tomas.krchnak@millennium.sk>
2019-01-16 12:52:36 +0100

    QRCode PoC - https://github.com/codebude/QRCoder

---
+++ TestQrCode/Program.cs
@@ -0,0 +1,41 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static QRCoder.PayloadGenerator;
+
+namespace TestQrCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Test();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ex: " + ex);
+            }
+
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+
+        private static void Test()
+        {
+            Url generator = new Url("https://github.com/codebude/QRCoder/");
+            string payload = generator.ToString();
+
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(payload, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            var qrCodeAsBitmap = qrCode.GetGraphic(20);
+
+            qrCodeAsBitmap.Save("test.bmp");
+        }
+    }
+}
