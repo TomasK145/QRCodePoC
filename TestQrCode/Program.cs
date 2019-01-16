492982afcfd8f3964b22041f2a55e73275cc12b5
Tomas Krchnak <tomas.krchnak@millennium.sk>
2019-01-16 13:57:12 +0100

    ulozenie ako jpeg

--- TestQrCode/Program.cs
+++ TestQrCode/Program.cs
@@ -1,6 +1,7 @@
 ﻿using QRCoder;
 using System;
 using System.Collections.Generic;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -35,7 +36,7 @@ namespace TestQrCode
             QRCode qrCode = new QRCode(qrCodeData);
             var qrCodeAsBitmap = qrCode.GetGraphic(20);
 
-            qrCodeAsBitmap.Save("test.bmp");
+            qrCodeAsBitmap.Save("test.jpeg", ImageFormat.Jpeg);
         }
     }
 }
